25a382f851c1b6baf471bd96546848d057912c56
Caikainan <cai20000528@gmail.com>
2023-04-21 19:35:32 +0200

    Character state machine
    Character state machine

---
+++ Assets/Scripts/ControlCharacter.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 1.0f;
+    public float turnSpeed = 1.0f;
+    public float transitionTime = 0.2f;
+    public float gravity = -9.8f;
+
+    private CharacterController controller;
+    private Animator animator;
+    private Vector2 currentInput;
+    private float verticalVelocity;
+ 
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+ 
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 targetInput = new Vector2(moveHorizontal, moveVertical);
+        currentInput = Vector2.Lerp(currentInput, targetInput, Time.deltaTime / transitionTime);
+
+        animator.SetFloat("H", currentInput.x);
+        animator.SetFloat("V", currentInput.y);
+
+        if(controller.isGrounded)
+        {
+            verticalVelocity = 0;
+
+
+        }
+        else
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+        Vector3 gravityVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(gravityVector * Time.deltaTime);
+
+     }
+
+
+
+}
